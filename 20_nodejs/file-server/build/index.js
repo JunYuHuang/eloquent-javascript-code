7db8ad44676528e088d4e6fb85ab425ea0dd19d7
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-03-25 21:04:45 -0700

    Update file server projecjt for Ch.20

--- 20_nodejs/file-server/build/index.js
+++ 20_nodejs/file-server/build/index.js
@@ -3,8 +3,7 @@
 Chapter 20: Node.js - File Server
 
 This is an HTTP server that allows remote access to a file system.
-It allows web apps to store and share data or it can give people shared
-access to some files.
+It allows web apps to store and share data or it can give people shared access to some files.
 */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -42,39 +41,39 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var http_1 = require("http");
-/*
-`methods` is an object that stores async HTTP method handlers.
-Theses handlers take a request object as argument returns a promise
-that resolves to an object that describes the response.
-*/
+var node_http_1 = require("node:http");
 var methods = Object.create(null);
 /*
-This starts a server that returns 405 error response by default.
-If the handler's promise is rejected, it sends an error response to the client.
-If the handler's promise is resolved, we destructure some of its properties.
-`body` may be `null`, a string, a buffer, or a readable stream. If it is a
-readable stream, we call its `pipe` method to forward all its content to a writable stream. Otherwise, we pass `body` to the response's `end` method.
+Creates a server that returns a 500 status by default.
 */
-(0, http_1.createServer)(function (request, response) {
+(0, node_http_1.createServer)(function (request, response) {
     var handler = methods[request.method] || notAllowed;
     handler(request)
         .catch(function (error) {
         if (error.status != null)
             return error;
-        return { body: String(error), status: 500 };
+        return {
+            body: String(error),
+            status: 500,
+        };
     })
         .then(function (_a) {
         var body = _a.body, _b = _a.status, status = _b === void 0 ? 200 : _b, _c = _a.type, type = _c === void 0 ? "text/plain" : _c;
         response.writeHead(status, { "Content-Type": type });
-        if (body && body.pipe)
+        var isReadableStream = body && body.pipe;
+        if (isReadableStream) {
+            /*
+            Forward its content to a writable stream.
+            */
             body.pipe(response);
-        else
+        }
+        else {
+            /*
+            `body` is null, a string or a buffer.
+            */
             response.end(body);
+        }
     });
 }).listen(8000);
 function notAllowed(request) {
@@ -87,135 +86,18 @@ function notAllowed(request) {
         });
     });
 }
-/*
-`urlPath` is a function that returns the relative path for a given URL
-after parsing the URL and removing the `%20`-style escape codes.
-
-It uses the `resolve` function and throws an error if the given URL is
-below the current working directory (`cwd`) for security purposes. Otherwise
-it returns the correct path string.
-*/
-var url_1 = require("url");
-var path_1 = require("path");
+var node_path_1 = require("node:path");
 var baseDirectory = process.cwd();
 function urlPath(url) {
-    var pathname = (0, url_1.parse)(url).pathname;
-    var path = (0, path_1.resolve)(decodeURIComponent(pathname).slice(1));
+    var pathname = new URL(url, "http://d").pathname;
+    var path = (0, node_path_1.resolve)(decodeURIComponent(pathname).slice(1));
     if (path != baseDirectory &&
-        !path.startsWith(baseDirectory + path_1.sep)) {
-        throw { status: 403, body: "Forbidden" };
+        !path.startsWith(baseDirectory + node_path_1.sep)) {
+        throw {
+            status: 403,
+            body: "Forbidden",
+        };
     }
     return path;
 }
-/*
-`methods.GET` is a method that returns a list of files in a directory or a
-file's content when reading a regular file. It uses the `mime` package to
-return the correct `Content-Type` header in the response for a given file.
-It returns a 404 status code if the file doesn't exist.
-*/
-var fs_1 = require("fs");
-var promises_1 = require("fs/promises");
-var mime_1 = __importDefault(require("mime"));
-methods.GET = function (request) {
-    return __awaiter(this, void 0, void 0, function () {
-        var path, stats, error_1;
-        var _a;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
-                case 0:
-                    path = urlPath(request.url);
-                    _b.label = 1;
-                case 1:
-                    _b.trys.push([1, 3, , 4]);
-                    return [4 /*yield*/, (0, promises_1.stat)(path)];
-                case 2:
-                    stats = _b.sent();
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_1 = _b.sent();
-                    if (error_1.code != "ENOENT")
-                        throw error_1;
-                    return [2 /*return*/, { status: 404, body: "File not found" }];
-                case 4:
-                    if (!stats.isDirectory()) return [3 /*break*/, 6];
-                    _a = {};
-                    return [4 /*yield*/, (0, promises_1.readdir)(path)];
-                case 5: return [2 /*return*/, (_a.body = (_b.sent()).join("\n"), _a)];
-                case 6: return [2 /*return*/, {
-                        body: (0, fs_1.createReadStream)(path),
-                        type: mime_1.default.getType(path)
-                    }];
-            }
-        });
-    });
-};
-/*
-`methods.DELETE` deletes a directory or file. Even if the file cannot be found,
-we return a success HTTP status code of 204 since the request is technically
-already done and because the HTTP standard encourages requests to be idempotent.
-Idempotent means making the same request multiple times produces the same
-result as making it once.
-*/
-var promises_2 = require("fs/promises");
-methods.DELETE = function (request) {
-    return __awaiter(this, void 0, void 0, function () {
-        var path, stats, error_2;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    path = urlPath(request.url);
-                    _a.label = 1;
-                case 1:
-                    _a.trys.push([1, 3, , 4]);
-                    return [4 /*yield*/, (0, promises_1.stat)(path)];
-                case 2:
-                    stats = _a.sent();
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_2 = _a.sent();
-                    if (error_2.code != "ENOENT")
-                        throw error_2;
-                    return [2 /*return*/, { status: 204 }];
-                case 4:
-                    if (!stats.isDirectory()) return [3 /*break*/, 6];
-                    return [4 /*yield*/, (0, promises_2.rmdir)(path)];
-                case 5:
-                    _a.sent();
-                    return [3 /*break*/, 8];
-                case 6: return [4 /*yield*/, (0, promises_2.unlink)(path)];
-                case 7:
-                    _a.sent();
-                    _a.label = 8;
-                case 8: return [2 /*return*/, { status: 204 }];
-            }
-        });
-    });
-};
-/*
-`methods.PUT` creates or updates an existing file. We don't check if it exists here since we'll just overwrite it if it does.
-*/
-var fs_2 = require("fs");
-function pipeStream(from, to) {
-    return new Promise(function (resolve, reject) {
-        from.on("error", reject);
-        to.on("error", reject);
-        to.on("finish", resolve);
-        from.pipe(to);
-    });
-}
-methods.PUT = function (request) {
-    return __awaiter(this, void 0, void 0, function () {
-        var path;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    path = urlPath(request.url);
-                    return [4 /*yield*/, pipeStream(request, (0, fs_2.createWriteStream)(path))];
-                case 1:
-                    _a.sent();
-                    return [2 /*return*/, { status: 204 }];
-            }
-        });
-    });
-};
 //# sourceMappingURL=index.js.map
--- 20_nodejs/file-server/src/index.ts
+++ 20_nodejs/file-server/src/index.ts
@@ -2,158 +2,184 @@
 Chapter 20: Node.js - File Server
 
 This is an HTTP server that allows remote access to a file system.
-It allows web apps to store and share data or it can give people shared
-access to some files.
+It allows web apps to store and share data or it can give people shared access to some files.
 */
 
-import { createServer, IncomingMessage } from "http";
-
-/*
-`methods` is an object that stores async HTTP method handlers.
-Theses handlers take a request object as argument returns a promise
-that resolves to an object that describes the response.
-*/
+import { IncomingMessage, createServer } from "node:http";
 
 const methods = Object.create(null);
 
 /*
-This starts a server that returns 405 error response by default.
-If the handler's promise is rejected, it sends an error response to the client.
-If the handler's promise is resolved, we destructure some of its properties.
-`body` may be `null`, a string, a buffer, or a readable stream. If it is a
-readable stream, we call its `pipe` method to forward all its content to a writable stream. Otherwise, we pass `body` to the response's `end` method.
+Creates a server that returns a 500 status by default.
+- A RESTful API for files instead of database records.
+- Maps root (from `/`) file requests to their actual stored location in `/public/`. E.g., `GET /file.txt` retrieves the file stored at `/public/file.txt`.
 */
+createServer((request: IncomingMessage, response) => {
+  /*
+  `handler` is an async function that returns a promise.
+  It follows the error-first handling Node.js convention for async functions.
+  */
+  let handler = methods[request.method!] || notAllowed;
 
-createServer((request, response) => {
-    let handler = methods[request.method!] || notAllowed;
-    handler(request)
-        .catch((error) => {
-            if (error.status != null) return error;
-            return { body: String(error), status: 500 };
-        })
-        .then(({ body, status = 200, type = "text/plain" }) => {
-            response.writeHead(status, { "Content-Type": type });
-            if (body && body.pipe) body.pipe(response);
-            else response.end(body);
-        });
+  handler(request)
+    // handles rejected `request` promise
+    .catch((error) => {
+      if (error.status != null) return error;
+
+      return {
+        body: String(error),
+        status: 500,
+      }
+    })
+    // handles fulfilled `request` promise
+    .then(({ body, status = 200, type = "text/plain" }) => {
+      response.writeHead(status, { "Content-Type": type });
+
+      const isReadableStream = body && body.pipe;
+      if (isReadableStream) {
+        /*
+        Forward its content to a writable stream.
+        */
+        body.pipe(response);
+      } else {
+        /*
+        `body` is null, a string or a buffer.
+        */
+        response.end(body);
+      }
+    });
 }).listen(8000);
 
-async function notAllowed(request) {
-    return {
-        status: 405,
-        body: `Method ${request.method} not allowed.`
-    }
+async function notAllowed(request: IncomingMessage) {
+  return {
+    status: 405,
+    body: `Method ${request.method} not allowed.`
+  }
 }
 
 /*
-`urlPath` is a function that returns the relative path for a given URL
-after parsing the URL and removing the `%20`-style escape codes.
-
-It uses the `resolve` function and throws an error if the given URL is 
-below the current working directory (`cwd`) for security purposes. Otherwise
-it returns the correct path string.
+Gets the file path associated with a request URL.
 */
-
-import { parse } from "url";
-import { resolve, sep } from "path";
+import { parse } from "node:url";
+import { resolve, sep } from "node:path";
 
 const baseDirectory = process.cwd();
 
-function urlPath(url) {
-    let { pathname } = parse(url);
-    let path = resolve(decodeURIComponent(pathname!).slice(1));
-    if (path != baseDirectory &&
-        !path.startsWith(baseDirectory + sep)) {
-        throw { status: 403, body: "Forbidden" };
-    }
-    return path;
-}
-
 /*
-`methods.GET` is a method that returns a list of files in a directory or a
-file's content when reading a regular file. It uses the `mime` package to
-return the correct `Content-Type` header in the response for a given file.
-It returns a 404 status code if the file doesn't exist.
+To avoid such problems, urlPath uses the resolve function from the node:path module, which resolves relative paths. It then verifies that the result is below the working directory. The process.cwd function (where cwd stands for “current working directory”) can be used to find this working directory. The sep binding from the node:path package is the system’s path separator—a backslash on Windows and a forward slash on most other systems. When the path doesn’t start with the base directory, the function throws an error response object, using the HTTP status code indicating that access to the resource is forbidden.
 */
 
-import { createReadStream } from "fs";
-import { stat, readdir } from "fs/promises";
-import mime from "mime";
+/*
+Example 1:
+- input: 'https://example.org/abc/xyz?123'
+- output: 'D:\\GitHubProjects\\eloquent-javascript-code\\20_nodejs\\file-server\\abc\\xyz'
+*/
+function urlPath(url: string) {
+  let { pathname } = new URL(url, "http://d");
+  let path = resolve(decodeURIComponent(pathname).slice(1));
+  if (
+    path != baseDirectory &&
+    !path.startsWith(baseDirectory + sep)
+  ) {
+    throw {
+      status: 403,
+      body: "Forbidden",
+    };
+  }
+  return path;
+}
+
+
+
+/*
+GET `/:filePath` route handler.
+- throws error if file does not exist
+- returns array of file names in `path` if `path` is a directory
+- returns file (data) if `path` is a file
+*/
+
+import { ReadStream, Stats, WriteStream, createReadStream } from "node:fs";
+import { stat, readdir } from "node:fs/promises";
+import { lookup } from "mime-types";
 
 methods.GET = async function (request: IncomingMessage) {
-    let path = urlPath(request.url);
-    let stats;
-    try {
-        stats = await stat(path);
-    } catch (error: any) {
-        if (error.code != "ENOENT") throw error;
-        return { status: 404, body: "File not found" };
-    }
-    if (stats.isDirectory()) return { body: (await readdir(path)).join("\n") };
+  let path = urlPath(request.url!);
+  let stats: Stats;
+
+  try {
+    stats = await stat(path);
+  } catch (error: any) {
+    if (error.code !== "ENOENT") throw error;
     return {
-        body: createReadStream(path),
-        type: mime.getType(path)
+      status: 404,
+      body: "File not found"
     };
+  }
+
+  if (stats.isDirectory()) {
+    return {
+      body: (await readdir(path)).join("\n")
+    };
+  }
+  return {
+    body: createReadStream(path),
+    type: lookup(path)
+  }
 }
+
+
 
 /*
-`methods.DELETE` deletes a directory or file. Even if the file cannot be found,
-we return a success HTTP status code of 204 since the request is technically
-already done and because the HTTP standard encourages requests to be idempotent.
-Idempotent means making the same request multiple times produces the same 
-result as making it once.
+DELETE `/:filePath` route handler
+- Returns success status code 204 (no content) whether or not file exists to comply with HTTP idempotent standard.
+- Idempotent = Making the same request multiple times always returns same result as doing it once.
 */
 
-import { rmdir, unlink } from "fs/promises";
+import { rmdir, unlink } from "node:fs/promises";
 
-methods.DELETE = async function (request) {
-    let path = urlPath(request.url);
-    let stats;
-    try {
-        stats = await stat(path);
-    } catch (error: any) {
-        if (error.code != "ENOENT") throw error;
-        return { status: 204 };
-    }
-    if (stats.isDirectory()) await rmdir(path);
-    else await unlink(path);
+methods.DELETE = async function (request: IncomingMessage) {
+  let path = urlPath(request.url!);
+  let stats: Stats;
+
+  try {
+    stats = await stat(path);
+  } catch (error: any) {
+    if (error.code !== "ENOENT") throw error;
     return { status: 204 };
+  }
+
+  stats.isDirectory() ? await rmdir(path) : await unlink(path);
+
+  /*
+  When an HTTP response does not contain any data, the status code 204 (“no content”) can be used to indicate this.
+  */
+  return { status: 204 };
 }
+
+
 
 /*
-`methods.PUT` creates or updates an existing file. We don't check if it exists here since we'll just overwrite it if it does.
+PUT `/:filePath` route handler
+- Overwrites file it if exists already.
 */
 
-import { createWriteStream } from "fs";
+/*
+When something goes wrong when opening the file, createWriteStream will still return a stream, but that stream will fire an "error" event. The stream from the request may also fail, for example if the network goes down. So we wire up both streams’ "error" events to reject the promise. When pipe is done, it will close the output stream, which causes it to fire a "finish" event. That’s the point where we can successfully resolve the promise (returning nothing).
+*/
 
-function pipeStream(from, to) {
-    return new Promise((resolve, reject) => {
-        from.on("error", reject);
-        to.on("error", reject);
-        to.on("finish", resolve);
-        from.pipe(to);
-    });
+import { createWriteStream } from "node:fs";
+
+function pipeStream(from: IncomingMessage, to: WriteStream) {
+  return new Promise((resolve, reject) => {
+    from.on("error", reject);
+    to.on("error", reject);
+    to.on("finish", resolve);
+    from.pipe(to);
+  });
 }
 
-methods.PUT = async function (request) {
-    let path = urlPath(request.url);
-    await pipeStream(request, createWriteStream(path));
-    return { status: 204 };
-}
-
-// MKCOL extension from `Directory Creation` exercise
-import { mkdir } from "fs/promises";
-
-methods.MKCOL = async function (request) {
-    let path = urlPath(request.url);
-    let stats;
-    try {
-        stats = await stat(path);
-    } catch (error: any) {
-        if (error.code != "ENOENT") throw error;
-        await mkdir(path);
-        return { status: 204 };
-    }
-    if (stats.isDirectory()) return { status: 204 };
-    return { status: 400, body: "Not a directory" };
+methods.PUT = async function (request: IncomingMessage) {
+  let path = urlPath(request.url!);
+  await pipeStream(request, createWriteStream(path));
+  return { status: 204 };
 }
