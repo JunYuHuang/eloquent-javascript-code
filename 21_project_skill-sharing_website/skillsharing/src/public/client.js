713757a39509abfbef59e0cd4a2d6f1b3efabea4
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-03-30 08:27:46 -0700

    Update comments in Skillsharing project's client.js

--- 21_project_skill-sharing_website/skillsharing/src/public/client.js
+++ 21_project_skill-sharing_website/skillsharing/src/public/client.js
@@ -3,11 +3,11 @@
 // ACTIONS
 
 /*
-The application state consists of the list of talks and the name of the user, and we’ll store it in a {talks, user} object. We don’t allow the user interface to directly manipulate the state or send off HTTP requests. Rather, it may emit actions that describe what the user is trying to do.
+The application state consists of the list of talks and the name of the user, and we’ll store it in a `{talks, user}` object. We don’t allow the user interface to directly manipulate the state or send off HTTP requests. Rather, it may emit actions that describe what the user is trying to do.
 
-The handleAction function takes such an action and makes it happen. Because our state updates are so simple, state changes are handled in the same function.
+The `handleAction` function takes such an action and makes it happen. Because our state updates are so simple, state changes are handled in the same function.
 
-We’ll store the user’s name in localStorage so that it can be restored when the page is loaded.
+We’ll store the user’s name in `localStorage` so that it can be restored when the page is loaded.
 */
 
 function handleAction(state, action) {
@@ -54,7 +54,7 @@ function handleAction(state, action) {
 }
 
 /*
-The actions that need to involve the server make network requests, using fetch, to the HTTP interface described earlier. We use a wrapper function, fetchOK, which makes sure the returned promise is rejected when the server returns an error code.
+The actions that need to involve the server make network requests, using `fetch`, to the HTTP interface described earlier. We use a wrapper function, `fetchOK`, which makes sure the returned promise is rejected when the server returns an error code.
 */
 
 function fetchOK(url, options) {
@@ -73,7 +73,7 @@ function talkURL(title) {
 }
 
 /*
-When the request fails, we don’t want to have our page just sit there, doing nothing without explanation. So we define a function called reportError, which at least shows the user a dialog that tells them something went wrong.
+When the request fails, we don’t want to have our page just sit there, doing nothing without explanation. So we define a function called `reportError`, which at least shows the user a dialog that tells them something went wrong.
 */
 
 function reportError(error) {
@@ -102,11 +102,11 @@ function renderUserField(name, dispatch) {
 }
 
 /*
-The elt function used to construct DOM elements is the one we used in Chapter 19.
+The `elt` function used to construct DOM elements is the one we used in Chapter 19.
 
-One of the main things that interface components do is creating DOM structure. We again don’t want to directly use the verbose DOM methods for that, so here’s a slightly expanded version of the elt function:
+One of the main things that interface components do is creating DOM structure. We again don’t want to directly use the verbose DOM methods for that, so here’s a slightly expanded version of the `elt` function:
 
-The main difference between this version and the one we used in Chapter 16 is that it assigns properties to DOM nodes, not attributes. This means we can’t use it to set arbitrary attributes, but we can use it to set properties whose value isn’t a string, such as onclick, which can be set to a function to register a click event handler.
+The main difference between this version and the one we used in Chapter 16 is that it assigns properties to DOM nodes, not attributes. This means we can’t use it to set arbitrary attributes, but we can use it to set properties whose value isn’t a string, such as `onclick`, which can be set to a function to register a click event handler.
 
 This allows this convenient style for registering event handlers.
 */
@@ -126,7 +126,7 @@ function elt(type, props, ...children) {
 /*
 A similar function is used to render talks, which include a list of comments and a form for adding a new comment.
 
-The "submit" event handler calls form.reset to clear the form’s content after creating a "newComment" action.
+The `"submit"` event handler calls `form.reset` to clear the form’s content after creating a `"newComment"` action.
 
 When creating moderately complex pieces of DOM, this style of programming starts to look rather messy. To avoid this, people often use a templating language, which allows you to write your interface as an HTML file with some special markers to indicate where dynamic elements go. Or they use JSX, a non-standard JavaScript dialect that allows you to write something very close to HTML tags in your program as if they are JavaScript expressions. Both of these approaches use additional tools to pre-process the code before it can be run, which we will avoid in this chapter.
 */
@@ -220,13 +220,13 @@ function renderTalkForm(dispatch) {
 // POLLING
 
 /*
-To start the app we need the current list of talks. Since the initial load is closely related to the long polling process—the ETag from the load must be used when polling—we’ll write a function that keeps polling the server for /talks and calls a callback function when a new set of talks is available.
+To start the app we need the current list of talks. Since the initial load is closely related to the long polling process—the `ETag` from the load must be used when polling—we’ll write a function that keeps polling the server for `/talks` and calls a callback function when a new set of talks is available.
 
-This is an async function so that looping and waiting for the request is easier. It runs an infinite loop that, on each iteration, retrieves the list of talks—either normally or, if this isn’t the first request, with the headers included that make it a long polling request.
+This is an `async` function so that looping and waiting for the request is easier. It runs an infinite loop that, on each iteration, retrieves the list of talks—either normally or, if this isn’t the first request, with the headers included that make it a long polling request.
 
-When a request fails, the function waits a moment and then tries again. This way, if your network connection goes away for a while and then comes back, the application can recover and continue updating. The promise resolved via setTimeout is a way to force the async function to wait.
+When a request fails, the function waits a moment and then tries again. This way, if your network connection goes away for a while and then comes back, the application can recover and continue updating. The promise resolved via `setTimeout` is a way to force the async function to wait.
 
-When the server gives back a 304 response, that means a long polling request timed out, so the function should just immediately start the next request. If the response is a normal 200 response, its body is read as JSON and passed to the callback, and its ETag header value is stored for the next iteration.
+When the server gives back a `304` response, that means a long polling request timed out, so the function should just immediately start the next request. If the response is a normal `200` response, its body is read as JSON and passed to the callback, and its `ETag` header value is stored for the next iteration.
 */
 
 async function pollTalks(update) {
